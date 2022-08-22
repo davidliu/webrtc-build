ebf8f2096045a525b013bb65cf4c7412eb8e1427
davidliu <davidliu@deviange.net>
2022-08-22 17:22:07 +0900

    removing test from main builds

--- build.ios.sh
+++ build.ios.sh
@@ -2,5 +2,5 @@
 
 set -ex
 cd `dirname $0`
-python3 run.py build ios --webrtc-overlap-ios-build-dir --commit "$1" --test
+python3 run.py build ios --webrtc-overlap-ios-build-dir --commit "$1"
 python3 run.py package ios --webrtc-overlap-ios-build-dir
